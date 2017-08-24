b80b048212e43e50cf83add9e11a0fe2c21b79dc
benzj <jbcbenz@gmail.com>
2017-08-24 23:00:48 +0800

    first ruby job

---
+++ decision.rb
@@ -0,0 +1,15 @@
+def decision(par1)
+print(par1)
+par1=par1
+if(par1%2==0)
+print("Number is Even")
+else
+print("Number is Odd")
+end
+end
+#Main
+num = gets
+num1 = num.to_i
+decision(num1)
+#Function
+
